9f2fbec745c92c93efb689de28088111e071a3e3
fava9410 <xxalexvargas@gmail.com>
2019-09-01 22:33:22 -0500

    some error handling

--- RSP/views.py
+++ RSP/views.py
@@ -11,51 +11,65 @@ from .serializer import MatchSerializer, MatchDetailSerializer
 
 @csrf_exempt
 def start(request):
+    error = []
     if request.method == "POST":
-        m = Match.objects.create(number_of_rounds = 1)
-        game = {
-                'player1':{'name':request.POST.get('player1'),'score':0},
-                'player2':{'name':request.POST.get('player2'),'score':0},
-                'round':1,
-                'match_id':m.id}
-        request.session['game'] = game
-        return HttpResponseRedirect('/rsp/choose_move')
-    return render(request,"RSP/new_game.html")
+        try:
+            m = Match.objects.create(number_of_rounds = 1)
+            game = {
+                    'player1':{'name':request.POST.get('player1').upper(),'score':0},
+                    'player2':{'name':request.POST.get('player2').upper(),'score':0},
+                    'round':1,
+                    'match_id':m.id}
+            request.session['game'] = game
+            return HttpResponseRedirect('/rsp/choose_move')
+        except:
+            error.append("the game couldnt be created, please restart and try again")
+    return render(request,"RSP/new_game.html",{'error':error})
 
 @csrf_exempt
 def choose_move(request):
     game = request.session.get('game')
+    error = []
     context = {}
     context['choices'] = ['Rock','Scissors','Paper']
 
     if request.method == "POST":
-        game = request.session.get('game')
+        try:
+            game = request.session.get('game')
 
-        p1 = Player(**game['player1'])
-        p2 = Player(**game['player2'])
+            p1 = Player(**game['player1'])
+            p2 = Player(**game['player2'])
 
-        p1.choice = MoveFactory().create_move(request.POST.get("player1_choice"))
-        p2.choice = MoveFactory().create_move(request.POST.get("player2_choice"))
+            p1.choice = MoveFactory().create_move(request.POST.get("player1_choice"))
+            p2.choice = MoveFactory().create_move(request.POST.get("player2_choice"))
 
-        round = Round(p1,p2, game['round'],game['match_id'])
-        round.decide_winner()
-        Match_Detail.objects.create(**round.round_history)
-        round.clean_choices()
+            round = Round(p1,p2, game['round'],game['match_id'])
+            round.decide_winner()
+            Match_Detail.objects.create(**round.round_history)
+            round.clean_choices()
 
-        game['player1'] = p1.__dict__
-        game['player2'] = p2.__dict__
-        game['round'] = round.round + 1
-        game['match_id'] = round.match
-        request.session['game'] = game
+            game['player1'] = p1.__dict__
+            game['player2'] = p2.__dict__
+            game['round'] = round.round + 1
+            game['match_id'] = round.match
+            request.session['game'] = game
+        except:
+            error.append('Something happened in this round, this game wont be stored. \
+                        Please start a new one.')
 
-        if round.winner == True:
-            match = Match.objects.get(id=round.match)
-            match.number_of_rounds=round.round
-            match.winner=round.pwin.name
-            match.save()
-            return render(request,"RSP/winner.html",{'winner':round.pwin.name})
+        try:
+            if round.winner == True:
+                match = Match.objects.get(id=round.match)
+                match.number_of_rounds=round.round
+                match.winner=round.pwin.name
+                match.save()
+                return render(request,"RSP/winner.html",{'winner':round.pwin.name})
+        except:
+            error.append('An error has occurred updating the match and the winner, \
+                        this match wont count')
 
     context['game'] = game
+    context['error'] = error
     return render(request,"RSP/choose_move.html",context)
 
 class MatchesList(generics.ListAPIView):
@@ -72,6 +86,9 @@ def matches_history(request):
     return render(request,"RSP/matches.html")
 
 def top_leaders(request):
-    qs = Match.objects.values('winner').annotate(victories=Count('winner')).order_by('-victories')
-    data = {'data':list(qs)}
+    try:
+        qs = Match.objects.values('winner').annotate(victories=Count('winner')).order_by('-victories')
+        data = {'data':list(qs)}
+    except:
+        data = {'data':[]}
     return JsonResponse(data, safe=False)
--- static/js/RSP/choose_move.js
+++ static/js/RSP/choose_move.js
@@ -1,9 +1,15 @@
 $( document ).ready(function() {
+
   table_match_detail = $('#match_detail').DataTable({
     destroy: true,
     ajax:"/rsp/match_detail/"+$("#match").text().split("#")[1]+"/?format=datatables",
     serverSide:true,
     searching: false
+  });
+
+  $('#match_detail').on('error.dt',function(e, settings, techNote, message) {
+    alert("Something happened, you will be redirect to the main page, sorry");
+    window.location="/home";
   });
 
   $("#playes_choice").steps({
